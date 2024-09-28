31c8c4261c9a414c9956a809c679120a00e2fab9
David Worms <david@adaltas.com>
2024-09-28 22:46:32 +0200

    test: ts conversion

--- test/option.strip.ts
+++ test/option.strip.ts
@@ -1,11 +1,11 @@
 import pad from "../lib/index.js";
 
 describe("option.strip", function () {
-  it("strip if text longer than length", function () {
+  it("strip left if text longer than length", function () {
     pad(2, "abcdef", { strip: true }).should.eql("ef");
   });
 
-  it("strip if text longer than length", function () {
+  it("strip right if text longer than length", function () {
     pad("abcdef", 2, { strip: true }).should.eql("ab");
   });
 });
