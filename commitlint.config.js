2296aa81d54cab42fecc514d81d7dbbf0f7caaa2
David Worms <david@adaltas.com>
2024-09-29 01:24:43 +0200

    build: eslint and prettier

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["build", "chore", "ci", "docs", "feat", "fix", "refactor", "test"],
+    ],
+  },
+};
---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from "globals";
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import mocha from "eslint-plugin-mocha";
+import prettier from "eslint-plugin-prettier/recommended";
+
+export default [
+  {
+    ignores: ["dist/**"],
+  },
+  {
+    languageOptions: { globals: { ...globals.node } },
+  },
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  mocha.configs.flat.recommended,
+  prettier,
+];
