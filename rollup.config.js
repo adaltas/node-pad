d36d998990590608ebc9f5beb1af49467a1f17df
David Worms <david@adaltas.com>
2024-09-29 02:36:43 +0200

    build: rollup in esm and cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,43 @@
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "rollup-plugin-typescript2";
+import { dts } from "rollup-plugin-dts";
+import commonjs from "@rollup/plugin-commonjs";
+import del from "rollup-plugin-delete";
+// Resolve wcwidth to be included in the bundle;
+// Prevent "Unresolved dependencies" warning
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-export default {
-  input: "lib/index.js",
-  output: [
-    {
-      file: `dist/pad.umd.js`,
-      name: "mixme",
-      format: "umd",
-    },
-    {
-      file: `dist/pad.cjs.js`,
-      format: "cjs",
-    },
-    {
-      file: `dist/pad.esm.js`,
-      format: "esm",
-    },
-  ],
-  plugins: [
-    commonjs({
-      namedExports: {
-        // left-hand side can be an absolute path, a path
-        // relative to the current directory, or the name
-        // of a module in node_modules
-        "node_modules/wcwidth/index.js": ["wcwidth"],
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        dir: `dist/cjs`,
+        format: "cjs",
+        sourcemap: true,
+        entryFileNames: "[name].cjs",
       },
-    }),
-    resolve(),
-  ],
-};
+      {
+        dir: `dist/esm`,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    // external: ["mixme", "toposort"],
+    plugins: [
+      del({ targets: "dist/*" }),
+      nodeResolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "tsconfig.src.json",
+        useTsconfigDeclarationDir: true,
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: "./dist/types/index.d.ts",
+    output: [{ file: "dist/types/index.d.ts" }],
+    plugins: [dts(), del({ targets: "dist/types", hook: "buildEnd" })],
+  },
+];
